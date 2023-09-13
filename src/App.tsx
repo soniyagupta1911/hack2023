1efad7942b237af41997eb66aeec315774315d23
Rishabh Kalra <rishabhkalra@microsoft.com>
2023-09-13 11:55:42 +0530

    code optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+
 import { Configuration, OpenAIApi } from 'openai';
 import {WebApi, getPersonalAccessTokenHandler} from 'azure-devops-node-api';
 import './App.css';
@@ -15,47 +15,75 @@ function App() {
   });
   const openai = new OpenAIApi(configuration);
 
-  function ViewPullRequests(pullRequestId: number, GitRepo: string) {
+  // Get a GitHttpClient to talk to the Git endpoints
+  async function ViewPullRequests(pullRequestId: number, GitRepo: string) {
     debugger;
     let authHandler = getPersonalAccessTokenHandler("rjq3qtv6yrgf567gpg3ua6ibl4nnqrcegwm23up7tphoc7yymqzq");
     let connection = new WebApi("https://microsoft.visualstudio.com/Universal%20Store", authHandler);
-  
-    // Get a GitHttpClient to talk to the Git endpoints
-    connection.getGitApi().then((GitClient)=>{ GitClient.getPullRequest(GitRepo, pullRequestId).then((pullRequests:GitPullRequest) => {
-      GitClient.getPullRequestCommits(GitRepo, pullRequestId).then(commits => {
-          commits.forEach(commit => {
-              console.log(`${commit.commitId?.substring(0, 8)} ${commit.comment}`);
-              GitClient.getChanges(commit.commitId!, GitRepo).then(changes => {
-                  changes.changes?.forEach(change => {
-                      console.log(`${change.changeType}: ${change.item?.path}`);
-                  });
-              });
-          });
+    try {
+      const GitClient = await connection.getGitApi();
+      const pullRequests = await GitClient.getPullRequest(GitRepo, pullRequestId);
+      const commits = await GitClient.getPullRequestCommits(GitRepo, pullRequestId);
+      commits.forEach(async commit => {
+        console.log(`commit id : ${commit.commitId} / commit comment : ${commit.comment}`);
+        const changes = await GitClient.getChanges(commit.commitId!, GitRepo);
+        changes.changes?.forEach(change => {
+          console.log(`${change.changeType}: ${change.item?.path}`);
       });
-}).catch(error => console.error(error));});
+    });
+    } catch (e) {
+      console.error('catched error from outermost try catch, ', e);
+    }
+// Get a GitHttpClient to talk to the Git endpoints
+//     connection.getGitApi()
+//     .then((GitClient)=>{ GitClient.getPullRequest(GitRepo, pullRequestId).then((pullRequests:GitPullRequest) => {
+//       GitClient.getPullRequestCommits(GitRepo, pullRequestId).then(commits => {
+//           commits.forEach(commit => {
+//               console.log(`${commit.commitId?.substring(0, 8)} ${commit.comment}`);
+//               GitClient.getChanges(commit.commitId!, GitRepo).then(changes => {
+//                   changes.changes?.forEach(change => {
+//                       console.log(`${change.changeType}: ${change.item?.path}`);
+//                   });
+//               });
+//           });
+//       });
+// }).catch(error => console.error(error));});
+}
 
-  const handleSumit = (e:any) => {
+  const handleSumit = async (e:any) => {
     ViewPullRequests(9530365,"https://microsoft.visualstudio.com/Universal%20Store/_git/PSX.L2O.SalesPlatformUX.MSXExperience")
     e.preventDefault();
     setLoading(true);
-
-    openai
-      .createCompletion({
+    try {
+      const response = await openai.createCompletion({
         model: 'text-davinci-003',
         prompt: generatePrompt(text),
         temperature: 0.6,
         max_tokens: 100,
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          setLoading(false);
-          setSummarizedText(res.data.choices[0].text!);
-        }
-      })
-      .catch((err) => {
-        console.error(err, 'An error occurred');
-        setLoading(false);
       });
+      if (response.status === 200) {
+        setLoading(false);
+        setSummarizedText(response.data.choices[0].text!);
+      }
+    } catch (e) {
+      console.error(e, 'An error occurred');
+        setLoading(false);
+    }
+    // openai.createCompletion({
+    //     model: 'text-davinci-003',
+    //     prompt: generatePrompt(text),
+    //     temperature: 0.6,
+    //     max_tokens: 100,
+    //   })
+    //   .then((res) => {
+    //     if (res.status === 200) {
+    //       setLoading(false);
+    //       setSummarizedText(res.data.choices[0].text!);
+    //     }
+    //   })
+    //   .catch((err) => {
+        
+    //   });
   };
 
   const generatePrompt = (text:string) => {
@@ -103,7 +131,6 @@ function App() {
       </div>
     </div>
   );
-}
 }
 
 export default App;
